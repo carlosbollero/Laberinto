0da7a05a39680a5e008ea7f84de22529a4b94038
carlosbollero <carllosmbollero@gmail.com>
2014-04-17 19:48:02 -0300

    Modificado el archivo LectorArchivo.h

--- src/LectorArchivo.h
+++ src/LectorArchivo.h
@@ -5,14 +5,18 @@
 
 class LectorArchivo{
 private:
-
+	string ruta, nombre, linea;
 public:
-	/* pre: Debe recibir
+	/* pre: El nombre y la ubicación del archivo se suponen válidos.
+	 * post: Deja inicializado "linea" con la primer linea del archivo, 
+	 * 		"nombre" con el nombre del archivo y "ruta" con la ubicación del 
+	 * 		archivo.
+	 */
 	LectorArchivo();
 	~LectorArchivo();
 	/* pre: el archivo debe estar abierto para lectura.
 	 * post: devuelve el texto contenido en la linea actual y deja apuntada la 
-	 * siguiente linea.
+	 * 		siguiente linea.
 	 */
 	string getLinea(Tarchivo*);
 };
