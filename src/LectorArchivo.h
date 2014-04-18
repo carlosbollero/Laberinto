f8cba7c958204e714c0738a5853c8ed6b6265ed5
carlosbollero <carllosmbollero@gmail.com>
2014-04-17 21:55:01 -0300

    Agregados comentarios de cabeceras

--- src/LectorArchivo.h
+++ src/LectorArchivo.h
@@ -1,4 +1,8 @@
-
+/******************************************************************************
+ *                                                                            *
+ *                      Definición clase LectorArchivo                        *
+ *                                                                            *
+ ******************************************************************************/
 
 #include "tipos.h"
 
@@ -6,19 +10,29 @@
 class LectorArchivo{
 private:
 	string ruta, nombre, linea;
+	Tarchivo* ptrArchivo;
+
 public:
 	/* pre: El nombre y la ubicación del archivo se suponen válidos.
 	 * post: Deja inicializado "linea" con la primer linea del archivo, 
 	 * 		"nombre" con el nombre del archivo y "ruta" con la ubicación del 
 	 * 		archivo.
+	 *		Apunta "ptrArchivo" al archivo de texto.
 	 */
 	LectorArchivo();
 	/* Destructor.
 	 */
 	~LectorArchivo();
+
+	string getRuta();
+
+	string getNombre();
 	/* pre: el archivo debe estar abierto para lectura.
 	 * post: devuelve el texto contenido en la linea actual y deja apuntada la 
 	 * 		siguiente linea.
 	 */
-	string getLinea(Tarchivo*);
+	string getLinea(Tarchivo* archivo);
+
+
+	void
 };
